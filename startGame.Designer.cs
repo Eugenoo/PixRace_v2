d08bd00f5e384705e7dcb6af5d9d97e228b842dd
PC-Windows-Filip <filipxfx@gmail.com>
2020-01-23 22:07:33 +0100

    22:07 comm

--- startGame.Designer.cs
+++ startGame.Designer.cs
@@ -38,7 +38,17 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.playerCar = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car3 = new System.Windows.Forms.PictureBox();
+            this.gameOverLabel = new System.Windows.Forms.Label();
+            this.goToMenu = new System.Windows.Forms.Button();
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.pointsTimer = new System.Windows.Forms.Timer(this.components);
+            this.pointsLabel = new System.Windows.Forms.Label();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.speedTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.stripe1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.stripe2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.stripe3)).BeginInit();
@@ -47,7 +57,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).BeginInit();
             this.SuspendLayout();
             // 
             // stripe1
@@ -128,14 +141,109 @@
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox3
+            // playerCar
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Maroon;
-            this.pictureBox3.Location = new System.Drawing.Point(333, 533);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 75);
-            this.pictureBox3.TabIndex = 8;
-            this.pictureBox3.TabStop = false;
+            this.playerCar.BackColor = System.Drawing.Color.Maroon;
+            this.playerCar.Location = new System.Drawing.Point(333, 533);
+            this.playerCar.Name = "playerCar";
+            this.playerCar.Size = new System.Drawing.Size(50, 75);
+            this.playerCar.TabIndex = 8;
+            this.playerCar.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.Color.DarkMagenta;
+            this.car1.Location = new System.Drawing.Point(56, 533);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(50, 75);
+            this.car1.TabIndex = 9;
+            this.car1.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.Purple;
+            this.car2.Location = new System.Drawing.Point(56, 316);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(50, 75);
+            this.car2.TabIndex = 10;
+            this.car2.TabStop = false;
+            // 
+            // car3
+            // 
+            this.car3.BackColor = System.Drawing.Color.Purple;
+            this.car3.Location = new System.Drawing.Point(56, 88);
+            this.car3.Name = "car3";
+            this.car3.Size = new System.Drawing.Size(50, 75);
+            this.car3.TabIndex = 11;
+            this.car3.TabStop = false;
+            // 
+            // gameOverLabel
+            // 
+            this.gameOverLabel.AutoSize = true;
+            this.gameOverLabel.BackColor = System.Drawing.Color.Teal;
+            this.gameOverLabel.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameOverLabel.Location = new System.Drawing.Point(122, 265);
+            this.gameOverLabel.Margin = new System.Windows.Forms.Padding(5);
+            this.gameOverLabel.Name = "gameOverLabel";
+            this.gameOverLabel.Size = new System.Drawing.Size(233, 60);
+            this.gameOverLabel.TabIndex = 12;
+            this.gameOverLabel.Text = "GAME OVER";
+            this.gameOverLabel.Visible = false;
+            // 
+            // goToMenu
+            // 
+            this.goToMenu.BackColor = System.Drawing.Color.Teal;
+            this.goToMenu.Location = new System.Drawing.Point(180, 345);
+            this.goToMenu.Name = "goToMenu";
+            this.goToMenu.Size = new System.Drawing.Size(114, 23);
+            this.goToMenu.TabIndex = 13;
+            this.goToMenu.Text = "MAIN MENU";
+            this.goToMenu.UseVisualStyleBackColor = false;
+            this.goToMenu.Visible = false;
+            this.goToMenu.Click += new System.EventHandler(this.goToMenu_Click);
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.BackColor = System.Drawing.Color.Teal;
+            this.resumeButton.Location = new System.Drawing.Point(199, 395);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(75, 23);
+            this.resumeButton.TabIndex = 14;
+            this.resumeButton.Text = "RESUME";
+            this.resumeButton.UseVisualStyleBackColor = false;
+            this.resumeButton.Visible = false;
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // pointsTimer
+            // 
+            this.pointsTimer.Enabled = true;
+            this.pointsTimer.Interval = 1000;
+            this.pointsTimer.Tick += new System.EventHandler(this.Points_Tick);
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Location = new System.Drawing.Point(393, 31);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(56, 13);
+            this.pointsLabel.TabIndex = 15;
+            this.pointsLabel.Text = "LETS GO!";
+            this.pointsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(370, 657);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(79, 13);
+            this.speedLabel.TabIndex = 16;
+            this.speedLabel.Text = "SPEED 0 MPH";
+            this.speedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // speedTimer
+            // 
+            this.speedTimer.Enabled = true;
+            this.speedTimer.Tick += new System.EventHandler(this.speed_Tick);
             // 
             // startGame
             // 
@@ -143,7 +251,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(484, 689);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.speedLabel);
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.resumeButton);
+            this.Controls.Add(this.goToMenu);
+            this.Controls.Add(this.gameOverLabel);
+            this.Controls.Add(this.car3);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.playerCar);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox6);
@@ -154,6 +270,7 @@
             this.Controls.Add(this.stripe1);
             this.Name = "startGame";
             this.Text = "startGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.startGame_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.stripe1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stripe2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stripe3)).EndInit();
@@ -162,8 +279,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -178,6 +299,16 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox playerCar;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car3;
+        private System.Windows.Forms.Label gameOverLabel;
+        private System.Windows.Forms.Button goToMenu;
+        private System.Windows.Forms.Button resumeButton;
+        private System.Windows.Forms.Timer pointsTimer;
+        private System.Windows.Forms.Label pointsLabel;
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.Timer speedTimer;
     }
 }
--- startGame.cs
+++ startGame.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace PixRace_v2
 {
@@ -21,34 +23,226 @@ namespace PixRace_v2
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            moveline(2);
+            enemy(gamespeed);
+            moveline(gamespeed);
+            gameover();
+            
+            
+            
         }
 
+        Random rand = new Random();
+        int x, y;
+
+
+        void enemy(int speed)
+        {
+            int speedlocal = speed;
+            if (car1.Top >= 699)
+            {
+                x = rand.Next(30, 404);
+                y = -100;
+                car1.Location = new Point(x, y);
+            }
+            else { car1.Top += (speedlocal / 4); }
+
+            if (car2.Top >= 699)
+            {
+                x = rand.Next(30, 404);
+                y = -100;
+                car2.Location = new Point(x, y);
+            }
+            else { car2.Top += (speedlocal / 4); }
+
+            if (car3.Top >= 699)
+            {
+                x = rand.Next(30, 404);
+                y = -100;
+                car3.Location = new Point(x, y);
+            }
+            else { car3.Top += (speedlocal / 4); }
+        }
+
+        void savescore()
+        {
+            string[] lines = { "First line", "Second line", "Third line" };
+            System.IO.File.WriteAllLines(@"C:\Users\admin\source\repos\scores.txt",lines);
+        }
+        void moveline2(int speed,PictureBox formStripe)
+        {
+            int speedlocal2 = speed;
+
+            if (formStripe.Top >= 699)
+            { formStripe.Top = -85; }
+            else { formStripe.Top += (speedlocal2 / 2); }
+        }
         void moveline(int speed)
         {
-            if (stripe1.Top >= 699)
-            { stripe1.Top = -85; }
-            else { stripe1.Top += speed; }
+            int speedlocal = speed;
 
-            if (stripe2.Top >= 699)
-            { stripe2.Top = -85; }
-            else { stripe2.Top += speed; }
+            moveline2(speedlocal, stripe1);
+            // makes stripes go slower than aproaching enemy cars
 
-            if (stripe3.Top >= 699)
-            { stripe3.Top = -85; }
-            else { stripe3.Top += speed; }
-
-            if (stripe4.Top >= 699)
-            { stripe4.Top = -85; }
-            else { stripe4.Top += speed; }
-
+            moveline2(speedlocal, stripe2);
+            moveline2(speedlocal, stripe3);
+            moveline2(speedlocal, stripe4);
             //if (stripe5.Top >= 700)
             //{ stripe5.Top = -85; }
             //else { stripe5.Top += speed; }
 
 
         }
+        void endgame()
+        {
+            timer1.Enabled = false;
+            gameOverLabel.Visible = true;
+            resumeButton.Visible = true;
+            goToMenu.Visible = true;
+            pointsTimer.Enabled = false;
+            savescore();
+        }
+        void gameover()
+        {
+            if (playerCar.Bounds.IntersectsWith(car1.Bounds))
+            {
+                endgame();
+              
+
+            }
+            if (playerCar.Bounds.IntersectsWith(car2.Bounds))
+            {
+                endgame();
+                
+            }
+            if (playerCar.Bounds.IntersectsWith(car3.Bounds))
+            {
+                endgame();
+               
+            }
+        }
+        int gamespeed = 0;
+
+      
+        
+        
+        private void resumeButton_Click(object sender, EventArgs e)
+        {
+            restart();
+            savescore();
+            
+
+        }
+
+        private void restart()
+        {
+            gameOverLabel.Visible = false;
+            resumeButton.Visible = false;
+            goToMenu.Visible = false;
+            timer1.Enabled = true;
+
+           
+            startGame newGame = new startGame();             //This will be used if i didnt find how to restart in same window
+            newGame.Show();
+            Close();
 
 
+
+            /*timer1.Enabled = true;
+            playerCar.Location = new Point(333, 533);
+            car3.Location = new Point(56, 88);
+            car2.Location = new Point(56, 316);
+            car1.Location = new Point(56, 533);*/
+            
+        }
+       /* private void countPoints()
+        {
+            int points =0;
+            if(gamespeed == 0)
+            {
+                points += 0;
+            }
+            else
+            {
+               // points = gamespeed * 
+            }
+        }*/
+        private void goToMenu_Click(object sender, EventArgs e)
+        {
+            Form1 menu = new Form1();
+            menu.Show();
+            Close();
+        }
+
+        int points = 0;
+
+        private void Points_Tick(object sender, EventArgs e)
+        {
+
+            
+
+            if (gamespeed == 0)
+                {
+                    points += 0;
+                pointsLabel.Text = "Score: " + points ;
+                }
+                else
+                {
+                    points += gamespeed;
+                pointsLabel.Text = "Score: " + points ;
+                }
+
+        }
+
+       
+
+        private void speed_Tick(object sender, EventArgs e)
+        {
+            int speed = 0;
+            if (gamespeed == 0)
+               {
+                speed += 0;
+                speedLabel.Text = "Speed " + speed +" MPH";
+               }
+            else
+            {
+                speed += gamespeed;
+                speedLabel.Text = "Speed " + speed +" MPH";
+            }
+        }
+
+        
+
+        private void startGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            //moving left & right
+            if (e.KeyCode == Keys.Left)
+            {
+                if (playerCar.Left > 30)
+                    playerCar.Left -= 10;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (playerCar.Left < 404)
+                    playerCar.Left += 10;
+            }
+
+            //moving foward & back
+
+            if (e.KeyCode == Keys.Up)
+                if (gamespeed <= 32)
+                {
+                    gamespeed++;
+                }
+            if (e.KeyCode == Keys.Down)
+                if (gamespeed > 0)
+                {
+                    gamespeed--;
+                }
+
+
+
+
+
+        }
     }
 }
